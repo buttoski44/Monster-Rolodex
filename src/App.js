57c2a0988c1cba6fe71ac3ec5e5e2069e601d0ad
vishal sangole <wishalsangole4444@gmail.com>
2022-02-25 17:48:47 +0530

    searchbox

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import {Component} from 'react';
 import { Cardlist } from './components/card-list/card-list.component';
 import './App.css';
 import { Card } from './components/card/card.component';
+import { SearchBox } from './components/search-box/search-box.component';
 
 class App extends Component{
   constructor(){
     super();
 
     this.state = {
-      monster:[]
+      monster:[],
+      searchFeild : ""
     };
 
  
@@ -21,9 +23,18 @@ class App extends Component{
   }
 
   render(){
+
+    const {monster ,searchFeild} = this.state;
+    const filteredMonsters = monster.filter(monster => monster.name.toLowerCase().includes(searchFeild.toLowerCase()));
+
     return(
       <div className="App">
-        <Cardlist monsters ={this.state.monster}/>
+        <SearchBox placeholder="serach monsters" 
+          handleChange={e => this.setState({searchFeild : e.target.value}, 
+          () => console.log(e.target.value))}
+        />
+        
+        <Cardlist monsters ={filteredMonsters}/>
 
         {/* prop has children property - usefull. */}
       </div>
